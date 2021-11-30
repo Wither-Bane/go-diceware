4dc302bf2a7c9d8091d20e1fbd3dff18451ed92a
Dipo TWB <Wither-Bane@users.noreply.github.com>
2021-11-30 12:19:30 +0000

    added some hacky code to main to enable addition of randomly generated prefix and suffic for passphrases. going to test now

--- cmd/diceware/main.go
+++ cmd/diceware/main.go
@@ -6,6 +6,11 @@ import (
 	"os"
 
 	"github.com/sethvargo/go-diceware/diceware"
+
+	//DIPO-CODE
+	"crypto/rand"
+	"math/big"
+	//DIPO-CODE
 )
 
 var (
@@ -17,6 +22,30 @@ var (
 	stdout, stderr = os.Stdout, os.Stderr
 )
 
+//DIPO-CODE
+func gen_rand_num_prefix() int {
+	var min int64 = 10
+	var max int64 = 99
+
+	max_iterations := 100
+
+	var num int64 = 0
+
+	for i := 0; i < max_iterations; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(max-min))
+		if err != nil {
+			panic(err)
+		}
+
+		if num.Int64() >= min {
+			break
+		}
+	}
+	return int(num)
+}
+
+//DIPO-CODE
+
 func main() {
 	flag.Parse()
 
@@ -26,12 +55,24 @@ func main() {
 		os.Exit(2)
 	}
 
+	//DIPO-CODE
+
+	num1 := gen_rand_num_prefix()
+	num2 := gen_rand_num_prefix()
+
+	fmt.Fprint(stdout, num1)
+	fmt.Fprint(stdout, *flagSeparator)
+	//DIPO-CODE
 	for i, w := range list {
 		fmt.Fprint(stdout, w)
 		if i < len(list)-1 {
 			fmt.Fprint(stdout, *flagSeparator)
 		}
 	}
+	//DIPO-CODE
+	fmt.Fprint(stdout, *flagSeparator)
+	fmt.Fprint(stdout, num2)
+	//DIPO-CODE
 
 	if fi, _ := stdout.Stat(); fi == nil || (fi.Mode()&os.ModeCharDevice) != 0 {
 		fmt.Fprintln(stdout)
